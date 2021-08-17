5a0033cafcb1890736d1ab61aac4cb25e646e9b4
photainfire <zac.reid@ucdconnect.ie>
2021-08-17 19:37:45 +0100

    general changes
    not significant. (prototyping code)

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,8 @@
 use leto;
 fn main() {
-    let input = String::from("Hello, World! ❤️");
-    let x = leto::byte2char::Byte2Char::new(input.as_bytes()).last();
+    let input = String::from("Hello, World! 한 🤢😎💋🐱‍🏍");
+    let x:Vec<char> = leto::byte2char::Byte2Char::new(input.as_bytes()).collect();
+    let y:Vec<char> = input.chars().collect();
     println!("{:?}", x);
+    println!("{:?}", y);
 }
